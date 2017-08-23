27f36b2caf3eac16320d0af43a82106bf789ec2b
Drake Lambert <drake.askme2@gmail.com>
2017-08-23 11:46:30 -0500

    started adding course models

---
+++ Models/Course.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Schedule4Me.Models
+{
+	/// <summary>
+	/// An LSU course
+	/// </summary>
+	public class Course
+	{
+		/// <summary>
+		/// The prefix for this course
+		/// </summary>
+		public string Prefix { get; set; }
+
+		/// <summary>
+		/// The number for this course
+		/// </summary>
+		public int Number { get; set; }
+
+		public 
+	}
+}
---
+++ Models/Section.cs
@@ -0,0 +1,9 @@
+namespace Schedule4Me.Models
+{
+	public class Section
+	{
+		public int Number { get; set; }
+		public double StartHour { get; set; }
+		public double EndHour { get; set; }
+	}
+}
