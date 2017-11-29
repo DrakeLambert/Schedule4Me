7cdc92b99e3fe7caa22458b655e3ea36e324f373
Drake Lambert <drake.askme2@gmail.com>
2017-11-29 16:49:22 -0600

    separated classes into individual files

--- Models/CourseAI.cs
+++ Models/CourseAI.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace Schedule4Me.Models
@@ -111,67 +110,6 @@ namespace Schedule4Me.Models
         private static F Apply<T, F>(this T value, Func<T, F> function)
         {
             return function(value);
-        }
-    }
-
-    public class IdentifiableCourse
-    {
-        public string Prefix { get; set; }
-        public string Number { get; set; }
-        public string SectionNumber { get; set; }
-        public int Code { get; set; }
-        // List of Day,Time
-        public List<Tuple<int, int>> TimeIntervals { get; set; }
-
-        public IdentifiableCourse()
-        {
-            TimeIntervals = new List<Tuple<int, int>>();
-        }
-    }
-
-    public class Schedule : IEnumerable
-    {
-        // Dictionary of days 
-        // Dictionary of times
-        // List of courses
-        private Dictionary<int, Dictionary<int, List<IdentifiableCourse>>> _schedule;
-
-        public Schedule()
-        {
-            _schedule = new Dictionary<int, Dictionary<int, List<IdentifiableCourse>>>();
-            for (var i = 0; i < 7; i++)
-            {
-                _schedule.Add(i, new Dictionary<int, List<IdentifiableCourse>>());
-            }
-        }
-
-        public void Add(IdentifiableCourse course)
-        {
-            foreach (var interval in course.TimeIntervals)
-            {
-                var day = _schedule[interval.Item1];
-                if (day.ContainsKey(interval.Item2))
-                {
-                    day[interval.Item2].Add(course);
-                }
-                else
-                {
-                    day.Add(interval.Item2, new List<IdentifiableCourse> { course });
-                }
-            }
-        }
-
-        public void AddRange(IEnumerable<IdentifiableCourse> courses)
-        {
-            foreach (var course in courses)
-            {
-                Add(course);
-            }
-        }
-
-        public IEnumerator GetEnumerator()
-        {
-            return _schedule.GetEnumerator();
         }
     }
 }
---
+++ Models/IdentifiableCourse.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace Schedule4Me.Models
+{
+    public class IdentifiableCourse
+    {
+        public string Prefix { get; set; }
+        public string Number { get; set; }
+        public string SectionNumber { get; set; }
+        public int Code { get; set; }
+        // List of Day,Time
+        public List<Tuple<int, int>> TimeIntervals { get; set; }
+
+        public IdentifiableCourse()
+        {
+            TimeIntervals = new List<Tuple<int, int>>();
+        }
+    }
+}
