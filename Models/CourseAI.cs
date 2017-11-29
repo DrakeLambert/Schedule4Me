a18e54dbebe7d24384efbf62043ade79b1f8ab26
Drake Lambert <drake.askme2@gmail.com>
2017-11-29 16:10:49 -0600

    added ability to create schedule data structure in memory

--- Models/CourseAI.cs
+++ Models/CourseAI.cs
@@ -23,31 +23,82 @@ namespace Schedule4Me.Models
                     };
                     foreach (var interval in section.timeIntervals)
                     {
-                        // interval.
-                        // iCourse.TimeIntervals.Add(())
+                        var times = StringIntervalToInt(interval.start, interval.end);
+
+                        foreach (var day in interval.days)
+                        {
+                            int dayInt;
+                            switch (day.ToLower())
+                            {
+                                case "sunday":
+                                    dayInt = 0;
+                                    break;
+                                case "monday":
+                                    dayInt = 1;
+                                    break;
+                                case "tuesday":
+                                    dayInt = 2;
+                                    break;
+                                case "wednesday":
+                                    dayInt = 3;
+                                    break;
+                                case "thursday":
+                                    dayInt = 4;
+                                    break;
+                                case "friday":
+                                    dayInt = 5;
+                                    break;
+                                case "saturday":
+                                    dayInt = 6;
+                                    break;
+                                default:
+                                    dayInt = 0;
+                                    break;
+                            }
+                            foreach (var time in times)
+                            {
+                                iCourse.TimeIntervals.Add(new Tuple<int, int>(dayInt, time));
+                            }
+                        }
                     }
+                    schedule.Add(iCourse);
                 }
             }
             return courses;
         }
 
-        // private List<int> StringIntervalToInt(string start, string end)
-        // {
+        private static List<int> StringIntervalToInt(string start, string end)
+        {
+            var startInt = StringTimeToInt(start);
+            var endInt = StringTimeToInt(end);
+            var times = new List<int>();
+            for (; startInt < endInt; startInt++)
+            {
+                times.Add(startInt);
+            }
+            return times;
+        }
 
-        // }
+        private static int StringTimeToInt(string time)
+        {
+            var hour = time.Substring(0, 2)
+                .Apply(Convert.ToInt32);
+            var minute = time.Substring(2, 2)
+                .Apply(Convert.ToInt32);
 
-        // private int StringTimeToInt(string time)
-        // {
-        //     var hour = time.Substring(0, 2)
-        //         .Apply(Convert.ToInt32);
-        //     var minute = time.Substring(2, 2)
-        //         .Apply(Convert.ToInt32);
-
-        //     if (time.EndsWith('N'))
-        //     {
-        //         hour
-        //     }
-        // }
+            hour += 1;
+            if (hour < 6 || time.EndsWith('N'))
+            {
+                // Afternoon
+                hour += 12;
+            }
+            hour *= 2;
+            if (minute <= 30)
+            {
+                return hour;
+            }
+            return hour + 1;
+        }
 
         private static F Apply<T, F>(this T value, Func<T, F> function)
         {
