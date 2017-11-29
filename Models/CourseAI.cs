aad8c411adb6677836d914571fb2a0a02aef31cd
Drake Lambert <drake.askme2@gmail.com>
2017-11-29 11:46:16 -0600

    added course AI that doesnt do anything at all

---
+++ Models/CourseAI.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace Schedule4Me.Models
+{
+    public static class CourseAI
+    {
+        public static List<Course> Schedule(this List<Course> courses)
+        {
+            return courses;
+        }
+    }
+}
+
+
+
+/*
+Planned approach
+
+1. Add all sections of course to calendar
+2. Add next
+ */
